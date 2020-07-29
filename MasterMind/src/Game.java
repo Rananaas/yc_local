432418ad52ffa4d642782f1d4d6b2fa274b889e3
Rananaas <ranascheerman@gmail.com>
2020-07-29 16:50:05 +0200

    commit4MM

--- MasterMind/src/Game.java
+++ MasterMind/src/Game.java
@@ -10,18 +10,25 @@ public class Game{
 	public static void startUp() { 
 	
 		System.out.println("WELKOM BIJ MASTERMIND");
+		System.out.println("NEED HELP? Hit h to read how the game works");
+		System.out.println("/////////////////////////////////////////////");
 		System.out.println("Speler 1 mag een code invoeren:");
 		Scanner codeToGuessScanner = new Scanner(System.in);
 		String codeToGuess = codeToGuessScanner.nextLine();
 		
 		if(codeToGuess.length() != 4) { 
-			System.out.println("CODE MUST BE 4 NUMBERS");
-			startUp();
+			
+			if(codeToGuess.equals("h")) { 
+				explainGame();
+			} else
+				System.out.println("CODE MUST BE 4 NUMBERS");
+				startUp();
 		}
 		else {
 		System.out.println("code to guess is:  "+ codeToGuess);
 		System.out.println("Hit S to start game with this code");
 		System.out.println("Hit q to enter other code");
+		System.out.println("Hit h to read how the game works");
 		} 
 		Scanner confirmCodeScanner = new Scanner(System.in);
 		String confirmCode = confirmCodeScanner.nextLine();
@@ -34,6 +41,23 @@ public class Game{
 		if(confirmCode.equals("q")) { 
 			startUp();
 		}
+		if(confirmCode.equals("h")) { 
+			explainGame();
+		}
+		
+	}
+
+	private static void explainGame() {
+		System.out.println("///////////////GAME MANUAL: /////////////////////");
+		System.out.println("Guess the secret code of 4 numbers");
+		System.out.println("You have 10 attempts to do so");
+		System.out.println("If an number is part of the code AND in the right place, a 'B' will be returned");
+		System.out.println("If an number is part of the code BUT NOT in the right place, a 'W' will be returned");
+		System.out.println("WATCH OUT! the order of how the B's and W's will be returned doesnt necessarily means"
+				+ "this is the correct order ");
+		System.out.println("//////////GOOD LUCK!////////////");
+		startUp();
+		
 		
 	}
 
--- MasterMind/src/playing.java
+++ MasterMind/src/playing.java
@@ -6,6 +6,7 @@ public class playing {
 	public static void letsgo(char[] codeToGuessArray) { 
 		System.out.println("PLAYING GAME!, code to guess will be" + Arrays.toString(codeToGuessArray)); 
 
+
 		//int blackPins = 0;
 		//int whitePins = 0;
 		int guessesLeft = 10;
@@ -88,6 +89,8 @@ public class playing {
 		
 		//System.out.println("Amount of whitePins: " + whitePins);
 		//System.out.println("Amount of blackPins: " + blackPins);
+		//hieronder ongesorteerde array, zo zie je meteen de plekken van welke B of A zijn
+		
 		System.out.println("UNSORTED: " + Arrays.toString(guessResultArray));
 		Arrays.sort(guessResultArray);
 		
